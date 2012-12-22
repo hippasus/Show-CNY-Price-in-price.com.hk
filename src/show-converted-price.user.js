0dbf37f8b47b280e6f2add080cc561b9c8337a9c
Hippasus, Chu <zhuyanggu@gmail.com>
2012-12-22 13:48:56 +0800

    pushlish v1.0
    1. multipy rate exchange providers (yahoo finance &
    rate-exchange.appspot.com) supported.
    2. Greasemonkey supported.

--- src/show-converted-price.user.js
+++ src/show-converted-price.user.js
@@ -1,36 +1,184 @@
 ﻿// ==UserScript==
-// @name        Show CNY price for price.com.hk
-// @namespace   hippasus
-// @description Show CNY price for price.com.hk
-// @include     http://www.price.com.hk/*
-// @version     0.2
-// @grant       none
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require     https://raw.github.com/josscrowcroft/accounting.js/master/accounting.min.js
+// @name		Show CNY price for price.com.hk
+// @namespace	hippasus
+// @description	Show CNY price for price.com.hk
+// @icon		https://github.com/hyacinth/Show-CNY-Price-in-price.com.hk/raw/master/src/img/icon128.png
+// @author		Hippasus Chu
+// @include		http://www.price.com.hk/*
+// @version		1.0
+// @grant		GM_getValue
+// @grant		GM_setValue
+// @grant		GM_deleteValue
+// @grant		GM_listValues
+// @grant		GM_xmlhttpRequest
+// @grant		GM_log
+// @grant		unsafeWindow
+// @require		http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
+// @require		https://raw.github.com/josscrowcroft/accounting.js/master/accounting.min.js
+
+// @run-at		document-end
 // ==/UserScript==
 $(function() {
 	"use strict"
 
-	var __version__ = '0.2',
+	var __version__ = '1.0',
 
 		currency_base, rates, _delay = 100,
 		RATE_PATTERN = /^\d+\.\d+/,
 
 		getRates = function() {
 
-			var rates_json = localStorage["rates"],
-				local_timestamp = localStorage["currency_timestamp"],
+			var STORAGE_RATES_KEY = "currency_rates",
+				STORAGE_CURRENCY_TIMESTAMP_KEY = "currency_timestamp",
+				STORAGE_CURRENCY_BASE_KEY = "currency_base",
+
+				firefoxGreeseMonkeyMode = (typeof GM_getValue !== 'undefined') && GM_getValue.toString && (GM_getValue.toString().indexOf("not supported") < 0),
+				loadStorage = function(key) {
+					if(firefoxGreeseMonkeyMode) {
+						return GM_getValue(key);
+					} else {
+						return localStorage[key];
+					}
+				},
+				saveStorage = function(key, val) {
+					if(firefoxGreeseMonkeyMode) {
+						GM_setValue(key, val);
+					} else {
+						localStorage[key] = val;
+					}
+				},
+
+				rates_json = loadStorage(STORAGE_RATES_KEY),
+				local_timestamp = parseInt(loadStorage(STORAGE_CURRENCY_TIMESTAMP_KEY), 10),
 				timestamp = (new Date()).getTime(),
 				// 1 hour cache duration
-				cache_duration = 1000 * 60 * 60;
+				cache_duration = 1000 * 60 * 60,
 
-			currency_base = localStorage["currency_base"];
+				makeGetRequest = function(url, successHanlder) {
+					var d = $.Deferred(),
 
-			if(rates_json) {
-				rates = JSON.parse(rates_json);
-			}
+						makeRequestXhrPromise = function(url) {
+							var promise;
+							if(firefoxGreeseMonkeyMode) {
+								var xhrDeferred = $.Deferred();
+								GM_xmlhttpRequest({
+									method: "GET",
+									url: url,
+									onload: function(response) {
+										var responseText = response.responseText,
+											data = JSON.parse(responseText);
+										xhrDeferred.resolveWith(null, [data]);
+									},
+									onerror: function(response) {
+										xhrDeferred.reject();
+									}
+								});
 
-			if(rates && currency_base && local_timestamp && (timestamp - local_timestamp < cache_duration)) {
+								promise = xhrDeferred.promise();
+							} else {
+								promise = $.ajax({
+									url: url,
+									type: 'GET'
+								});
+							}
+
+							return promise;
+						},
+						requestXHR = makeRequestXhrPromise(url);
+
+					requestXHR.done(function(data) {
+						try {
+							successHanlder(data);
+							d.resolve();
+						} catch(err) {
+							d.reject();
+						}
+					}).fail(function() {
+						d.reject();
+					});
+
+					return d.promise();
+				},
+				cacheRates = function() {
+					saveStorage(STORAGE_RATES_KEY, JSON.stringify(rates));
+					saveStorage(STORAGE_CURRENCY_BASE_KEY, currency_base);
+					saveStorage(STORAGE_CURRENCY_TIMESTAMP_KEY, timestamp.toString()); //Greasy monkey doesn't support long, use string instead
+				},
+				yahooFinanceRateExchangeProvider = function() {
+					var url = 'http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json',
+						d = makeGetRequest(url, function(data) {
+							var resources = data["list"]["resources"],
+								l = resources.length,
+								i = 0;
+
+							rates = {};
+							currency_base = 'USD';
+							rates[currency_base] = 1;
+
+							for(; i < l; i += 1) {
+								var resource = resources[i]["resource"],
+									fields = resource["fields"],
+									currency_name = fields["name"].substr(4),
+									rate_to_usd = fields["price"];
+
+								rates[currency_name] = rate_to_usd;
+							};
+
+							cacheRates();
+						});
+
+					return d;
+				},
+				gaeRateExchangeProvider = function() {
+					var url = 'http://rate-exchange.appspot.com/currency?from=HKD&to=CNY&q=1',
+						d = makeGetRequest(url, function(data) {
+							rates = {
+								'HKD': 1,
+								'CNY': data.rate
+							};
+							currency_base = 'HKD';
+
+							cacheRates();
+						});
+
+					return d;
+				},
+				rateExchangeProviders = [gaeRateExchangeProvider, yahooFinanceRateExchangeProvider],
+
+				fetchRates = function() {
+					var deferred = $.Deferred(),
+						i = 0,
+						providersCount = rateExchangeProviders.length,
+						doFetchRates = function() {
+							if(i >= providersCount) {
+								deferred.reject();
+								return;
+							}
+
+							var provider = rateExchangeProviders[i],
+								providerDeferred = provider();
+							providerDeferred.fail(function() {
+								i += 1;
+								doFetchRates();
+							}).done(function() {
+								deferred.resolve();
+							});
+						};
+
+					doFetchRates();
+					return deferred.promise();
+				},
+
+				isCacheValid = function() {
+					currency_base = loadStorage(STORAGE_CURRENCY_BASE_KEY);
+					if(rates_json) {
+						rates = JSON.parse(rates_json);
+					}
+
+					return rates && currency_base && local_timestamp && (timestamp - local_timestamp < cache_duration);
+				};
+
+			if(isCacheValid()) {
 				var d = $.Deferred(),
 					promise = d.promise();
 
@@ -39,42 +187,7 @@ $(function() {
 				return promise;
 			}
 
-			var requestXHR = $.ajax({
-				url: "http://rate-exchange.appspot.com/currency?from=HKD&to=CNY&q=1",
-				type: 'GET'
-			});
-			/*
-			var requestXHR = (function() {
-				var df = $.Deferred(),
-					xhr = new XMLHttpRequest();
-
-				xhr.open('GET', 'http://rate-exchange.appspot.com/currency?from=HKD&to=CNY&q=1', true);
-				xhr.onreadystatechange = function() {
-					if(xhr.readyState === 4) {
-						if(xhr.status !== 200 && xhr.status !== 304) {
-							df.reject();
-						}
-						df.resolve(JSON.parse(xhr.responseText));
-					}
-				};
-				xhr.send();
-
-				return df.promise();
-			}());*/
-
-			requestXHR.done(function(data) {
-				rates = {
-					'HKD': 1,
-					'CNY': data.rate
-				};
-				currency_base = 'HKD';
-
-				localStorage["rates"] = JSON.stringify(rates);
-				localStorage["currency_base"] = currency_base;
-				localStorage["currency_timestamp"] = timestamp;
-			});
-
-			return requestXHR;
+			return fetchRates();
 		},
 
 		eleID = 'cny-' + (new Date()).getTime(),
@@ -83,10 +196,11 @@ $(function() {
 			$(html).appendTo("body");
 		},
 
+		PRICE_ELEMENTS_FILTER = 'span.price, span.cheap, a.pgrid_price, a.rank_price, a.idx_rank_price',
 		timerID,
 		// register events
 		registerEventHandler = function() {
-			$("body").on('mouseover', 'span.price, span.cheap, a.pgrid_price, a.rank_price, a.idx_rank_price', function() {
+			$("body").on('mouseover', PRICE_ELEMENTS_FILTER, function() {
 				var $this = $(this),
 					pos = $this.offset(),
 					offset = {
